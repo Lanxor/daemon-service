00fbaccbcafbf4c84297b8025ad52ea8cd7c7250
OLIVIER Thomas <Thomas.olivier3@yahoo.fr>
2019-09-20 16:06:19 +0200

    Create manager.sh

---
+++ manager.sh
@@ -0,0 +1,55 @@
+usage() {
+  echo "$0 {start|stop|restart}"
+}
+
+check_process() {
+  if [ -f "$pidfile" ]; then
+    output=$(ps -p $(cat "`pwd`/$pidfile") --no-headers -o cmd)
+    if [ "$output" != "$cmd" ]; then
+      rm "$pidfile"
+    fi
+  fi
+}
+
+start() {
+  if [ ! -f "$pidfile" ]; then
+    ($cmd & echo $! > "$pidfile") &
+  fi
+}
+
+stop() {
+  if [ -f "$pidfile" ]; then
+    kill -9 $(cat "$pidfile")
+    rm "$pidfile"
+  fi
+}
+
+status() {
+  if [ -f "$pidfile" ]; then
+    echo "The daemon is running"
+  else
+    echo "The daemon is stopped."
+  fi
+}
+
+check_process
+case $1 in
+  "status")
+    status
+    ;;
+  "start")
+    start
+    ;;
+  "stop")
+    stop
+    ;;
+  "reload")
+    stop
+    start
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+exit 0
